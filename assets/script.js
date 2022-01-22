4c25c5008ea47e581d5412b44833b9ac1889147a
Jean McLeod <abricsltd@gmail.com>
2022-01-22 15:17:56 +0000

    link the script

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+
+$("#currentDay").text(moment().format("dddd MMM Do YYYY"))
+
+})
+//event listener for save btn
+//in event listener use this. to target the hour ID and text are class description
+// two variable one for id and one for 
+// to declare a variable eg hour ID this.parent().attrid()
